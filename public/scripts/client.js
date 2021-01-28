77fe5d001c3f27b73472242cdc4ac95c310b7078
Hsing-I Wang <hsing.sophie@gmail.com>
2021-01-28 01:28:41 +0000

    display tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,90 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+
+  const loadTweets = function () {
+    $.ajax({
+      url: `http://localhost:8080/tweets`,
+      method: 'GET',
+    })
+      .done((result) => {
+        renderTweets(result);
+      })
+      .fail(() => console.log('error'));
+  }
+
+  const renderTweets = function (tweets) {
+    for (const tweet of tweets) {
+      $('.tweets-display').prepend(createTweetElement(tweet));
+    };
+  };
+
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
+  const createTweetElement = function (tweet) {
+    const tweetObj =
+      `<aritcle class="tweet-display">
+        <header class="tweet-display-header">
+          <div>
+            <img src=${tweet.user.avatars} id="posted-user"></i>
+            <span>${tweet.user.name}</span>
+          </div>
+          <span class="hide">${tweet.user.handle}</span>
+        </header>
+        <div class="content">
+          <p>${escape(tweet.content.text)}</p>
+          <div class="hline"></div>
+        </div>
+        <footer class="tweet-display-footer">
+          <span>${calDate(tweet.created_at)} days ago</span>
+          <div class="tweet-display-icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-square"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </aritcle>`
+    return tweetObj;
+  };
+
+  const calDate = function (timeString) {
+    const date = moment(timeString);
+    return moment().diff(date, "days");
+    //const daysMs = Date.now() - timeString;
+    //return daysMs/86400000;
+  }
+
+  $('#tweet-form').on('submit', function (event) {
+    event.preventDefault();
+    const tweetInput = $(this).serialize();
+    if (tweetInput.length === 5) {
+      $('#error-message').text("please input the text!").slideDown();
+    } else if (tweetInput.length > 145) {
+      $('#error-message').text("please input less than or equal to 140 characters!").slideDown();
+    } else {
+      $.ajax({
+        url: `http://localhost:8080/tweets`,
+        method: 'POST',
+        data: tweetInput,
+      })
+        .done(() => {
+          loadTweets();
+          $("#tweet-text").val("");
+          const counterReset = $(this).closest('section').find('.counter');
+          counterReset[0].value = 140;
+          counterReset.css('color', '#808080');
+          $('#error-message').slideUp();
+        })
+        .fail(() => console.log('error'));
+    }
+  });
+
+
+  loadTweets();
+});
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,16 +4,14 @@ $(document).ready(function () {
   const maxCount = 140;
 
   textArea.on('input', function (event) {
-    //debugger
     let userInput = this.value;
     const countEle = $(event.target).closest('section').find('.counter');
     let countDisplay = countEle[0];
     countDisplay.value = maxCount - userInput.length;
     if (countDisplay.value < 0) {
-      countEle.css('color' , 'red');
-    } else{
-      countEle.css('color' , '#808080');
+      countEle.css('color', 'red');
+    } else {
+      countEle.css('color', '#808080');
     }
   });
-
 });
